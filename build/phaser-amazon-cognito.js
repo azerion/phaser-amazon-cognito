825353f7dd8319275cd1c6b250bc8a90014440f3
AleBles <a.bles@orangegames.com>
2017-01-23 16:46:38 +0100

    latest

--- build/phaser-amazon-cognito.js
+++ build/phaser-amazon-cognito.js
@@ -1,3 +1,12 @@
+/*!
+ * phaser-amazon-cognito - version 0.1.0 
+ * A Phaser plugin that adds User Login/Sync support trough Amazon Cognito Identity/Syn
+ *
+ * OrangeGames
+ * Build at 23-01-2017
+ * Released under MIT License 
+ */
+
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
